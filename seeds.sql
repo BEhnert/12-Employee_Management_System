d5860fa8fc98b70c78065722c68d31905242f33f
BEhnert <behnert77@gmail.com>
2021-02-11 14:02:49 -0600

    Updated sql files

--- seeds.sql
+++ seeds.sql
@@ -11,5 +11,3 @@ VALUES ("Sales Lead", 100000, 1),("Salesperson", 80000, 1),("Lead Engineer", 150
 INSERT INTO  employee (first_name, last_name, role_id, manager_id)
 VALUES ("John", "Doe", 1, 3),("Mike", "Chan", 2, 1),("Ashley", "Rodriguez", 3, null),("Kevin", "Tupik", 4,3),
 ("Malia","Brown",5,null),("Sarah","Lourd",6, null),("Tom","Allen",7, 6),("Christian", "Eckenrode", 8,2);
-
-
